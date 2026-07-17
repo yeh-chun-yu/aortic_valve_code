ceea4953f135ce94aae25927b4de25dbbfec4b76
yeh-chun-yu <80315393+yeh-chun-yu@users.noreply.github.com>
2026-07-17 13:35:57 +0800

    Update and rename test_p34.py to test_p5.py

--- test_p5.py
+++ test_p5.py
@@ -7,17 +7,17 @@ from pathlib import Path
 
 def main():
     model_list = [
-        "ultralytics/cfg/models/12/yolo12n-p34.yaml",
-        "ultralytics/cfg/models/12/yolo12s-p34.yaml",
-        "ultralytics/cfg/models/12/yolo12m-p34.yaml",
-        "ultralytics/cfg/models/12/yolo12l-p34.yaml",
-        "ultralytics/cfg/models/12/yolo12x-p34.yaml",
+        "ultralytics/cfg/models/12/yolo12n-p5.yaml",
+        "ultralytics/cfg/models/12/yolo12s-p5.yaml",
+        "ultralytics/cfg/models/12/yolo12m-p5.yaml",
+        "ultralytics/cfg/models/12/yolo12l-p5.yaml",
+        "ultralytics/cfg/models/12/yolo12x-p5.yaml",
     ]
 
     data_path = "data_full_fold_test.yaml"
 
     for model_path in model_list:
-        model_stem = Path(model_path).stem  # yolo12s-p34
+        model_stem = Path(model_path).stem  # yolo12s-p5
 
         for fold in range(5):
             print(f"\n===== Testing model: {model_stem} | fold{fold} =====")
